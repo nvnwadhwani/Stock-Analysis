4a72702227223fa0faa5f13205466e57f07bacd5
Navin Wadhwani <105993473+nvnwadhwani@users.noreply.github.com>
2023-07-17 17:01:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,69 +1,47 @@
 import streamlit as st
 import pandas as pd
-import yfinance as yf
 import talib
+import yfinance as yf
 
+def main():
+    # Load the stock data
+    ticker = st.text_input("Enter the stock ticker:")
+    data = yf.download(ticker, start="2022-01-01", end="2023-01-01")
 
-# Set the title and subtitle of the app
-st.title("Portfolio Analysis App")
-st.subheader("NSE India")
+    # Calculate the 200-day moving average
+    close = data["Close"]
+    MA200 = close.rolling(200).mean()
 
-# Create a sidebar for user input
-st.sidebar.title("Parameters")
-stocks = st.sidebar.text_input("Enter a list of stocks (comma-separated)", "AAPL, GOOGL, MSFT")
-rsi_threshold = st.sidebar.slider("RSI Threshold", 0, 100, 30)
-ma_200_threshold = st.sidebar.number_input("200-day MA Threshold", value=0)
-ma_50_threshold = st.sidebar.number_input("50-day MA Threshold", value=0)
+    # Calculate the MACD indicator
+    macd, macdsignal, macdhist = talib.MACD(close, fastperiod=12, slowperiod=26, signalperiod=9)
 
-# Fetch stock data from Yahoo Finance
-@st.cache_data
-def load_data(tickers):
-    data = yf.download(tickers, start="2022-01-01", end="2022-12-31")["Adj Close"]
-    return data
+    # Calculate the Stochastic Oscillator
+    stoch = talib.STOCH(close, high=data["High"], low=data["Low"], fastkperiod=14, slowkperiod=3, slowdperiod=3)
 
-# Load data for the selected stocks
-stock_data = load_data([stock.strip() for stock in stocks.split(",")])
+    # Create the Streamlit app
+    st.title("Stock Investment Strategy")
 
-# Calculate technical indicators (RSI, 200-day MA, 50-day MA)
-rsi = talib.RSI(stock_data, timeperiod=20)
-ma_200 = talib.SMA(stock_data, timeperiod=200)
-ma_50 = talib.SMA(stock_data, timeperiod=50)
+    # Plot the stock price
+    st.line_chart(close)
 
-# Display stock data and analysis
-for stock in stock_data.columns:
-    st.subheader(stock)
-    st.line_chart(stock_data[stock])
-    st.subheader("Technical Indicators")
-    st.line_chart(rsi[stock])
-    st.line_chart(ma_200[stock])
-    st.line_chart(ma_50[stock])
+    # Plot the 200-day moving average
+    st.line_chart(MA200)
 
-    # Check trading conditions
-    last_rsi = rsi[stock][-1]
-    last_ma_200 = ma_200[stock][-1]
-    last_ma_50 = ma_50[stock][-1]
+    # Plot the MACD indicator
+    st.line_chart(macd)
+    st.line_chart(macdsignal)
+    st.line_chart(macdhist)
 
-    if last_rsi > rsi_threshold and last_ma_200 > ma_200_threshold and last_ma_50 > ma_50_threshold:
-        st.success("Trading Signal: Buy")
-        # Plot buy signal
-        buy_signal_df = pd.DataFrame(stock_data[stock])
-        buy_signal_df["Buy Signal"] = buy_signal_df[stock].where(
-            (rsi[stock] > rsi_threshold) &
-            (ma_200[stock] > ma_200_threshold) &
-            (ma_50[stock] > ma_50_threshold),
-            None
-        )
-        st.line_chart(buy_signal_df)
+    # Plot the Stochastic Oscillator
+    st.line_chart(stoch["K"])
+    st.line_chart(stoch["D"])
 
-    else:
-        st.warning("Trading Signal: Sell")
-        # Plot sell signal
-        sell_signal_df = pd.DataFrame(stock_data[stock])
-        sell_signal_df["Sell Signal"] = sell_signal_df[stock].where(
-            (rsi[stock] <= rsi_threshold) |
-            (ma_200[stock] <= ma_200_threshold) |
-            (ma_50[stock] <= ma_50_threshold),
-            None
-        )
-        st.line_chart(sell_signal_df)
+    # Show the buy and sell signals
+    buy_signal = MA200 > close and macd > macdsignal and stoch["K"] < 20
+    sell_signal = MA200 < close and macd < macdsignal and stoch["K"] > 80
 
+    st.write("Buy signal:", buy_signal)
+    st.write("Sell signal:", sell_signal)
+
+if __name__ == "__main__":
+    main()
